44e273826d171eef0ee0661fa74c999773fc2b8e
jedaqsaul <jedaqsaul@gmail.com>
2025-05-20 19:58:20 +0300

    Completes the python init-self lab

--- lib/dog.py
+++ lib/dog.py
@@ -1,4 +1,26 @@
-#!/usr/bin/env python3
+# #!/usr/bin/env python3
 
 class Dog:
-    pass
+    def __init__(self, name, breed='Mutt'):
+        self.name=name
+        self.breed=breed
+        print(f"{name} is a {breed}")
+
+
+fido=Dog('Bosco')
+fido=Dog('Bosco','German Shepherd')
+
+
+# fido =Dog('Fido')
+# snoopy=Dog('Snoopy')
+# print(fido.name)
+# fido.owner="sophie"
+# print(fido.owner)
+
+# class Dog:
+#     def showing_self(self):
+#         return self
+    
+# fido=Dog()
+# print(fido.showing_self())
+# print(fido is fido.showing_self())
